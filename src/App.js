53c9b640d69ba36ba919ebb6fb386dae1996fcd3
scoquix <sebaszafranski@gmail.com>
2021-09-10 10:43:42 +0200

    first feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          It was changed!
+          New features available now!
         </p>
         <a
           className="App-link"
